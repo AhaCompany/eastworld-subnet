a8c66d7411b29f27c4f7a566036553446f4f53ee
Vu Minh Tam <vmintam@gmail.com>
2025-05-06 23:05:24 +0700

    change miner to senior with gemini

--- neurons/miner.py
+++ neurons/miner.py
@@ -3,14 +3,14 @@
 # Copyright © 2025 Eastworld AI
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 # and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of
 # the Software.
 
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
@@ -24,9 +24,7 @@ import bittensor as bt
 
 from eastworld.base.miner import BaseMinerNeuron
 from eastworld.protocol import Observation
-from eastworld.miner.junior import JuniorAgent
-
-# from eastworld.miner.senior import SeniorAgent
+from eastworld.miner.senior import SeniorAgent
 
 
 class WanderAgent(BaseMinerNeuron):
@@ -104,7 +102,7 @@ class WanderAgent(BaseMinerNeuron):
 if __name__ == "__main__":
     load_dotenv()
 
-    with JuniorAgent() as miner:  # Or try the SeniorAgent(slam_data=None)
+    with SeniorAgent(slam_data=None) as miner:  # Sử dụng SeniorAgent để đạt điểm cao hơn
         while True:
             bt.logging.info(f"Miner is running... {time.time()}")
             time.sleep(30)
